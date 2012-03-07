3838f52d359ee4711811de07ff5a36d0bbfae25f
NOTBAD <NOTBAD@192.168.0.102>
2012-03-07 20:34:13 +0200

    -ajaxController saving slideshows changed

--- src/goulu/notpowerpointBundle/Controller/ajaxController.php
+++ src/goulu/notpowerpointBundle/Controller/ajaxController.php
@@ -16,40 +16,113 @@ class ajaxController extends Controller
      /**
      * @Route("/ajax/slideshow/save")
      */
-    public function saveSlideshow()
+    public function saveSlideshowAction()
+    {   
+        $request      = $this->get('request');
+        $id           = $request->request->get('id', 'null');
+        if($id == 'null')
+        {
+            saveNewSlideshow();
+        }
+        else
+        {
+            UpdateExistingSlideshow($id);
+        }
+    }
+    
+    function saveNewSlideshow()
     {
         $slideshow = new Slideshow();
         $em = $this->getDoctrine()->getEntityManager();
-        
-        $request      = $this->get('request');
-        $id           = $request->request->get('id');
+        $request = $this->get('request');
         $name         = $request->request->get('name');
         $slides       = $request->request->get('slides');
         $creationdate = new \DateTime();
         $modifieddate = new \DateTime();
-        foreach($slides as $i => $value)
+        foreach($slides as $slide)
         {
-            $slide = new Slide();
-            $slide->setId(rand(0,9000));
-            $slide->setSlidenumber($i);
-            $slide->setContent($value);
-            $slide->setSlideshowid($id);
-            $slide->setCreationdate($creationdate);
-            $slide->setModifieddate($modifieddate);
-            $slide->setDeleted(0);
-            $slide->setShow(1);
-            $em->persist($slide);
+            saveNewSlide($slide);
         }
         $slideshow->setName($name);
-        $slideshow->setId($id);
+        $slideshow->setId(gen_uuid());
         $slideshow->setCreationdate($creationdate);
         $slideshow->setModifieddate($modifieddate);
         $slideshow->setPublished(1);
-        $slideshow->setDeleted(0);
-                
+        $slideshow->setDeleted(0);   
         $em->persist($slideshow);
         $em->flush();
-        return new Response('moi olen pena with id = ' . $id);
     }
+    
+    function updateExistingSlideshow($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $request = $this->get('request');
+        $name = $request->request->get('name');
+        $modifieddate = new \DateTime();
+        $slides = $request->request->get('slides');
+        $slideshow = $em->getRepository('goulunotpowerpointBundle:Slideshow')->find($id);
+        
+        if(!$slideshow)
+        {
+            throw $this->createNotFoundException('Slideshow not found');
+        }
+        else
+        {
+            $slideshow->setName($name);
+            $slideshow->setModifieddate($modifieddate);
+            $slideshow->setPublished(1);
+            $slideshow->setDeleted(0);
+            $em->persist($slideshow);
+            foreach($slides as $slide)
+            {
+                updateExistingSlide($slide);
+            }
+        }
+        $em->flush();
+    }
+    
+    function updateExistingSlide($slide)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $dbslide = $em->getRepository('goulunotpowerpointBundle:Slide')->find($slide['id']);
+        if(!$dbslide)
+        {
+            saveNewSlide($slide);
+        }
+        else
+        {
+            $dbslide->setSlidenumber($slide['ord']);
+            $dbslide->setContent($slide['content']);
+            $dbslide->setShow($slide['showable']);
+            $em->persist($dbslide);
+        }
+    }
+    
+    function saveNewSlide($slide)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $modifieddate = new \DateTime();
+        $creationdate = new \DateTime();
+        $slide = new Slide();
+        $slide->setId(gen_uuid());
+        $slide->setSlidenumber($slide['ord']);
+        $slide->setContent($slide['content']);
+        $slide->setSlideshowid(gen_uuid());
+        $slide->setCreationdate($creationdate);
+        $slide->setModifieddate($modifieddate);
+        $slide->setDeleted(0);
+        $slide->setShow($slide['showable']);
+        $em->persist($slide);
+    }
+    
+    function gen_uuid() {
+    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+        mt_rand( 0, 0xffff ),
+        mt_rand( 0, 0x0fff ) | 0x4000,
+        mt_rand( 0, 0x3fff ) | 0x8000,
+        mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+    );
+}
 }
 ?>

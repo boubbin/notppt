943c5fd99e50c2ba0dea50275efbc883b9e9b389
Joonas Harjukallio <harjukallio@gmail.com>
2012-03-18 12:28:59 +0200

    Xdebug config doc added

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new goulu\notpowerpointBundle\goulunotpowerpointBundle(),
-            new kakka\BlogBundle\kakkaBlogBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
